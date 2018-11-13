a4d78632ab031f2ddbd80331a97b4f9afda2bb3d
Simon VDB <simonvdbroeck@gmail.com>
2018-11-13 01:47:06 +0100

    Setup code generation on FE

---
+++ sick-fits/frontend/apollo.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  schemas: {
+    backend: {
+      endpoint: 'http://localhost:4444' // if not defined the schema will be downloaded from Apollo Engine
+    }
+  },
+  queries: [
+    // optional if you only have one schema
+    {
+      schema: 'backend', // reference the previously defined schema
+      includes: ['**/*.tsx'], // load queries from .tsx files
+      excludes: ['node_modules/**'] // don't include any matching files from node_modules
+    }
+  ]
+};
