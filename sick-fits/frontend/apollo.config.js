ddbd13cc2c810fd0902620eb57c583ea5ff05ae6
Simon VDB <simonvdbroeck@gmail.com>
2018-11-15 00:25:27 +0100

    Use latest apollo config

--- sick-fits/frontend/apollo.config.js
+++ sick-fits/frontend/apollo.config.js
@@ -1,15 +1,10 @@
 module.exports = {
-  schemas: {
-    backend: {
-      endpoint: 'http://localhost:4444' // if not defined the schema will be downloaded from Apollo Engine
-    }
-  },
-  queries: [
-    // optional if you only have one schema
-    {
-      schema: 'backend', // reference the previously defined schema
-      includes: ['**/*.tsx', '**/*.ts'], // load queries from .tsx files
-      excludes: ['node_modules/**'] // don't include any matching files from node_modules
-    }
-  ]
+  client: {
+    service: {
+      name: 'backend',
+      url: 'http://localhost:4444'
+    },
+    includes: ['**/*.tsx', '**/*.ts'], // load queries from .tsx files
+    excludes: ['node_modules/**'] // don't include any matching files from node_modules
+  }
 };
