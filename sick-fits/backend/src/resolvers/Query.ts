c91441891f9d593faa21e98f2e436d556e452b96
Simon VDB <simonvdbroeck@gmail.com>
2018-11-15 23:28:20 +0100

    Comment out moovly changes

--- sick-fits/backend/src/resolvers/Query.ts
+++ sick-fits/backend/src/resolvers/Query.ts
@@ -1,13 +1,13 @@
 import { QueryResolvers, MoovlyUserResolvers } from './resolvers-types';
 import { Ctx } from '../createServer';
-import { clients, configuration, middlewares } from '@moovly/clients';
+// import { clients, configuration, middlewares } from '@moovly/clients';
 
-const { Group, Licenses, Users } = clients;
+// const { Group, Licenses, Users } = clients;
 
-configuration.setToken('7483be3bcee246349a3dea580facf832046e02fc937246678b11af462e781332');
-configuration.setMiddlewares(middlewares.default);
+// configuration.setToken('7483be3bcee246349a3dea580facf832046e02fc937246678b11af462e781332');
+// configuration.setMiddlewares(middlewares.default);
 
-let start = 0;
+// let start = 0;
 const Query: QueryResolvers.Resolvers<Ctx> = {
   items(parent, args, ctx, info) {
     // @ts-ignore null || undefined issue
@@ -27,62 +27,62 @@ const Query: QueryResolvers.Resolvers<Ctx> = {
       aggregate: ctx.client.itemsConnection({ where }).aggregate(),
     }
   },
-  async seats() {
-    const start = Date.now();
-    const seats = await Licenses.getActiveSeats('7ecd40fb-b502-11e8-bd9b-0a2cf957548e');
+  // async seats() {
+  //   const start = Date.now();
+  //   const seats = await Licenses.getActiveSeats('7ecd40fb-b502-11e8-bd9b-0a2cf957548e');
 
-    const end = Date.now();
+  //   const end = Date.now();
 
-    console.log('Ran in ms:', end - start);
-    return seats;
-  },
-  // @ts-ignore
-  async group(parent, args, ctx, info) {
-    start = Date.now();
-    const seatsPr = await Licenses.getActiveSeats('7ecd40fb-b502-11e8-bd9b-0a2cf957548e');
+  //   console.log('Ran in ms:', end - start);
+  //   return seats;
+  // },
+  // // @ts-ignore
+  // async group(parent, args, ctx, info) {
+  //   start = Date.now();
+  //   const seatsPr = await Licenses.getActiveSeats('7ecd40fb-b502-11e8-bd9b-0a2cf957548e');
 
-    const membersPr = await Group.getGroupMemberships('7ecd40fb-b502-11e8-bd9b-0a2cf957548e', {
-      page: 1,
-      page_size: 100,
-    });
+  //   const membersPr = await Group.getGroupMemberships('7ecd40fb-b502-11e8-bd9b-0a2cf957548e', {
+  //     page: 1,
+  //     page_size: 100,
+  //   });
 
-    const [members1, seats] = await Promise.all([membersPr, seatsPr]);
+  //   const [members1, seats] = await Promise.all([membersPr, seatsPr]);
 
-    const members2 = await Group.getGroupMemberships('7ecd40fb-b502-11e8-bd9b-0a2cf957548e', {
-      page: 2,
-      page_size: 100,
-    });
+  //   const members2 = await Group.getGroupMemberships('7ecd40fb-b502-11e8-bd9b-0a2cf957548e', {
+  //     page: 2,
+  //     page_size: 100,
+  //   });
 
-    const members = members1.results.concat(members2.results);
+  //   const members = members1.results.concat(members2.results);
 
-    seats.forEach(s => {
-      const idx = members.findIndex(m => m.user_id === s.user_id);
+  //   seats.forEach(s => {
+  //     const idx = members.findIndex(m => m.user_id === s.user_id);
 
-      if(idx !== -1) {
-        // @ts-ignore
-        members[idx].seatInfo = s;
-      }
-    });
+  //     if(idx !== -1) {
+  //       // @ts-ignore
+  //       members[idx].seatInfo = s;
+  //     }
+  //   });
 
 
-    console.log(members.length);
-    const end = Date.now();
+  //   console.log(members.length);
+  //   const end = Date.now();
 
-    console.log('Ran in ms:', end - start);
-    console.log('fetch end:', end);
-    return members;
-  }
+  //   console.log('Ran in ms:', end - start);
+  //   console.log('fetch end:', end);
+  //   return members;
+  // }
 };
 
 export const MoovlyUser: MoovlyUserResolvers.Resolvers = {
-  async info(parent, args, ctx, info) {
-    const startLocal = Date.now();
-    console.log(`STARTED ${parent.user_id}: ${startLocal}`);
-    const res = await Users.getUserInformation(parent.user_id!);
-    console.log(`COMPLETED ${parent.user_id}: ${Date.now() - startLocal}`);
-    console.log(`TOTAL: ${Date.now() - start}`);
-    return res;
-  }
+  // async info(parent, args, ctx, info) {
+  //   const startLocal = Date.now();
+  //   console.log(`STARTED ${parent.user_id}: ${startLocal}`);
+  //   const res = await Users.getUserInformation(parent.user_id!);
+  //   console.log(`COMPLETED ${parent.user_id}: ${Date.now() - startLocal}`);
+  //   console.log(`TOTAL: ${Date.now() - start}`);
+  //   return res;
+  // }
 }
 
 export default Query;
