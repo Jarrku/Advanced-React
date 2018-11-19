6324338fa7d62a485959977ff360f372eeafaf91
Simon VDB <simonvdbroeck@gmail.com>
2018-11-19 20:34:23 +0100

    add userId on request

--- sick-fits/backend/src/index.ts
+++ sick-fits/backend/src/index.ts
@@ -2,10 +2,22 @@ import { config } from "dotenv";
 config({ path: ".env" });
 import * as cookieParser from 'cookie-parser';
 import createServer from "./createServer";
+import { verify } from "jsonwebtoken";
 
 const server = createServer();
 
 server.express.use(cookieParser());
+
+//decode the JWT so we can the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const { userId } = verify(token, process.env.APP_SECRET!) as { userId?: string };
+    // put userId on the request for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
