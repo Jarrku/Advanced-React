6456032b41c5f73e4086293f9da9b7ac083046ba
Simon VDB <simonvdbroeck@gmail.com>
2018-11-19 19:55:11 +0100

    implement signup

--- sick-fits/backend/src/resolvers/Mutation.ts
+++ sick-fits/backend/src/resolvers/Mutation.ts
@@ -1,5 +1,7 @@
 import { MutationResolvers } from "./resolvers-types";
 import { Ctx } from "../createServer";
+import { hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 const Mutations: MutationResolvers.Resolvers<Ctx> = {
   createItem(parent, args, ctx) {
@@ -23,6 +25,27 @@ const Mutations: MutationResolvers.Resolvers<Ctx> = {
     // TODO : check permission
 
     return ctx.client.deleteItem(where);
+  },
+  async signup(parent, args, ctx) {
+    const email = args.email.toLowerCase();
+
+    const password = await hash(args.password, 10);
+
+    const user = await ctx.client.createUser({
+        ...args,
+        email,
+        password,
+        permissions: { set: ['USER'] }
+    });
+
+    const token = sign({ userId: user.id }, process.env.APP_SECRET!);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    return user;
   }
 };
 
