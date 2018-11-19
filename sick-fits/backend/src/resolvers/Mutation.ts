1ed07e4b86d4d8021a6f19ede8282355ed902f9d
Simon VDB <simonvdbroeck@gmail.com>
2018-11-19 23:30:38 +0100

    implement reset Password flow on BE

--- sick-fits/backend/src/resolvers/Mutation.ts
+++ sick-fits/backend/src/resolvers/Mutation.ts
@@ -1,7 +1,18 @@
 import { MutationResolvers } from "./resolvers-types";
 import { Ctx } from "../createServer";
-import { hash, compare } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
+import { hash, compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { randomBytes } from "crypto";
+import { promisify } from "util";
+import { CookieOptions } from "express-serve-static-core";
+
+const ONE_HOUR = 1000 * 60 * 60;
+const ONE_YEAR = ONE_HOUR * 24 * 365;
+
+const COOKIE_SETTINGS: CookieOptions = {
+  httpOnly: true,
+  maxAge: ONE_YEAR,
+}
 
 const Mutations: MutationResolvers.Resolvers<Ctx> = {
   createItem(parent, args, ctx) {
@@ -10,7 +21,7 @@ const Mutations: MutationResolvers.Resolvers<Ctx> = {
       description: args.data.description || "default description",
       image: args.data.image || undefined,
       largeImage: args.data.largeImage || undefined,
-      price: args.data.price || 0,
+      price: args.data.price || 0
     });
   },
   updateItem(parent, args, ctx) {
@@ -32,48 +43,104 @@ const Mutations: MutationResolvers.Resolvers<Ctx> = {
     const password = await hash(args.password, 10);
 
     const user = await ctx.client.createUser({
-        ...args,
-        email,
-        password,
-        permissions: { set: ['USER'] }
+      ...args,
+      email,
+      password,
+      permissions: { set: ["USER"] }
     });
 
-    const token = sign({ userId: user.id }, process.env.APP_SECRET!);
-
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-    });
+    // Generate Token
+    const token = generateToken(user.id);
+    // Set cookie with the token
+    ctx.response.cookie("token", token, COOKIE_SETTINGS);
 
     return user;
   },
   async signin(parent, { email, password }, ctx, info) {
     // Check if there is a user.
     const user = await ctx.client.user({ email });
-    if(!user) {
+    if (!user) {
       throw new Error(`No such user found for email ${email}`);
     }
     // Check if password matches
     const valid = await compare(password, user.password);
-    if(!valid) {
-      throw new Error('Invalid password!');
+    if (!valid) {
+      throw new Error("Invalid password!");
     }
     // Generate Token
-    const token = sign({ userId: user.id }, process.env.APP_SECRET!);
+    const token = generateToken(user.id);
     // Set cookie with the token
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-    });
+    ctx.response.cookie("token", token, COOKIE_SETTINGS);
+
     // Return the user
     return user;
   },
   async signout(parent, args, ctx, info) {
-    ctx.response.clearCookie('token');
+    ctx.response.clearCookie("token");
     return {
-      message: 'Goodbye!'
+      message: "Goodbye!"
     };
+  },
+  async requestReset(parent, { email }, ctx, info) {
+    // Check if user exist
+    const user = await ctx.client.user({ email });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    const resetToken = (await promisify(randomBytes)(20)).toString("hex");
+    const resetTokenExpiry = Date.now() + ONE_HOUR;
+
+    const res = await ctx.client.updateUser({ data: { resetToken, resetTokenExpiry }, where: { email } });
+    console.log(res);
+    // Set Reset token & expiry
+    // Email reset token
+    return {
+      message: "Success"
+    };
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // Check if password match
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Passwords don't match");
+    }
+    // Check if its legit token && check expired
+    const [user] = await ctx.client.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - ONE_HOUR
+      }
+    });
+
+    if (!user) {
+      throw new Error("This token is either invalid or expired!");
+    }
+    // Hash new password
+    const password = await hash(args.password, 10);
+    // Save the new password and remove resetToken fields
+    const updatedUser = await ctx.client.updateUser({
+      where: {
+        id: user.id
+      },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    });
+
+    // Generate Token
+    const token = generateToken(user.id);
+    // Set cookie with the token
+    ctx.response.cookie("token", token, COOKIE_SETTINGS);
+
+    // return new user
+    return updatedUser;
   }
 };
+
+function generateToken(userId: string) {
+  return sign({ userId }, process.env.APP_SECRET!);
+}
 
 export default Mutations;
