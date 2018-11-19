7b8cd84bcf879ba15c76d6a2cb2726602e94480d
Simon VDB <simonvdbroeck@gmail.com>
2018-11-19 22:30:17 +0100

    toggle nav based on logged in or not

--- sick-fits/frontend/components/Nav.tsx
+++ sick-fits/frontend/components/Nav.tsx
@@ -3,30 +3,34 @@ import NavStyles from './styles/NavStyles';
 import User from './User/User';
 
 const Nav: React.SFC = () => (
-  <NavStyles>
-    <User>
-      {({ data }) => {
-        if (!data || !data.me) return null;
-
-        return <p>{data.me.name}</p>;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup!</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {data && data.me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+          </>
+        )}
+        {!data ||
+          (!data.me && (
+            <Link href="/signup">
+              <a>Sign In!</a>
+            </Link>
+          ))}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
