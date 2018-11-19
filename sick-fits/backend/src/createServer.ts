88baa490b3e94c08efaadec75590bef9d688359a
Simon VDB <simonvdbroeck@gmail.com>
2018-11-19 20:34:36 +0100

    Fix typing of Request instance

--- sick-fits/backend/src/createServer.ts
+++ sick-fits/backend/src/createServer.ts
@@ -5,10 +5,16 @@ import Mutation from './resolvers/Mutation';
 import db from './db';
 import { prisma } from './generated/prisma-client';
 import { ContextParameters } from 'graphql-yoga/dist/types';
+import { Request } from 'express-serve-static-core';
 
+
+interface CustomRequest {
+  userId?: string;
+}
 export interface Ctx extends ContextParameters {
   db: typeof db,
   client: typeof prisma,
+  request: Request & CustomRequest,
 }
 
 function createServer() {
